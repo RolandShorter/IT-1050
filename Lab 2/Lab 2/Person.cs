4b08a1a2ef373a97ab1f748655bf9682889fd9a8
Roland Shorter <rjshorter4034@twc.com>
2019-02-23 09:23:19 -0500

    new changes

---
+++ Lab 2/Lab 2/Person.cs
@@ -0,0 +1,24 @@
+﻿namespace Lab_2
+{
+    class Person
+    { 
+        public string FirstName;
+        public string LastName;
+        public int Age;
+        public string Spouse;
+   
+        public static int SumOfAllAges;
+
+        public string GetFullName()
+        {
+            return this.FirstName + " " + this.LastName;
+        }
+        public void PrintNameAndAge()
+        {
+            System.Console.WriteLine(this.GetFullName());
+            System.Console.WriteLine(this.Age);
+        }
+
+    
+    }
+}
---
+++ Lab 2/Lab 2/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace Lab_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Person p1;
+            p1 = new Person();
+            p1.Age = 49;
+            p1.FirstName = "";
+            p1.LastName = "";
+            p1.Spouse = 
+            
+        }
+    }
+}
