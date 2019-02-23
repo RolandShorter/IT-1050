6a656772f1d9100b4c9c16a6613b74c8c3d01108
Roland Shorter <rjshorter4034@twc.com>
2019-02-23 09:46:58 -0500

    material not needed

--- Lab 2/Lab 2/Program.cs
+++ Lab 2/Lab 2/Program.cs
@@ -6,10 +6,6 @@
         {
             Person p1;
             p1 = new Person();
-            p1.Age = 49;
-            p1.FirstName = "";
-            p1.LastName = "";
-            p1.Spouse = 
             
         }
     }
