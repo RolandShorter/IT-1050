b9b132e5bd62a2d1c5c71889e86930069bfedb09
Roland Shorter <rjshorter4034@twc.com>
2019-02-23 11:48:16 -0500

    added code

--- Lab 2/Lab 2/Person.cs
+++ Lab 2/Lab 2/Person.cs
@@ -18,6 +18,18 @@
             System.Console.WriteLine(this.GetFullName());
             System.Console.WriteLine(this.Age);
         }
+        public void GetInformation()
+        {
+            System.Console.Write("What is your First Name  ");
+            this.FirstName = System.Console.ReadLine();
+
+            System.Console.Write("What is your Last Name  ");
+            this.LastName = System.Console.ReadLine();
+
+            System.Console.Write("What is your Age  ");
+            this.Age = int.Parse (System.Console.ReadLine());
+
+        }
 
     
     }
--- Lab 2/Lab 2/Program.cs
+++ Lab 2/Lab 2/Program.cs
@@ -6,6 +6,7 @@
         {
             Person p1;
             p1 = new Person();
+            p1.GetInformation();
             
         }
     }
