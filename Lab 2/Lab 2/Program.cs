e408aaa7229212b259c50caae216ddacebacfe6c
Roland Shorter <rjshorter4034@twc.com>
2019-03-29 11:18:02 -0400

    updated

--- Lab 2/Lab 2/Program.cs
+++ Lab 2/Lab 2/Program.cs
@@ -8,12 +8,13 @@
             person1.GetUserInfo();
             person1.GetUserSpouseNameAndAge();
             person1.PrintNameAndAge();
+            person1.Spouse.PrintNameAndAge();
 
             Person person2 = new Person();
             person2.GetUserInfo();
             person2.GetUserSpouseNameAndAge();
             person2.PrintNameAndAge();
-
+            person2.Spouse.PrintNameAndAge();
             
             
             System.Console.WriteLine("Average Age is " + Person.AverageAge());
